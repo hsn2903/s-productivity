110cce8678f2b24b2656a693253f6c216d60cb8f
Hasan Çelik <hsn.2903@outlook.com>
2022-10-08 18:55:15 +0300

    add navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="container h-12 border-b border-blue-900 mx-auto">
+      <div className="flex justify-between items-center h-full">
+        <a href="#" className="inline-block text-slate-200">
+          Seyir
+        </a>
+        <button className="inline-block text-slate-200">open</button>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
